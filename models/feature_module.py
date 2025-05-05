a76602def2c964d465964148d75e1e17f56a618b
duccd <caodinhducuet@gmail.com>
2025-05-06 03:04:59 +0900

    update

--- models/feature_module.py
+++ models/feature_module.py
@@ -13,6 +13,21 @@ class FeatureModule(nn.Module):
         with open("label2vect.json", "r") as f:
             self.text_encoder = json.load(f)
         self.MAX_NUM_OBJECT = 8
+        self.TEXT_EMBEDDING_DIM = 300
+
+    def make_sphere(self, points):
+        centroid = np.mean(points, axis=0)
+        normalized_points = points - centroid
+        max_dist = np.max(np.linalg.norm(normalized_points, axis=1))
+        normalized_points /= max_dist
+        min_vals = np.min(points, axis=0)
+        max_vals = np.max(points, axis=0)
+        normalized_points = (points - min_vals) / (max_vals - min_vals)
+        mean = np.mean(points, axis=0)
+        std = np.std(points, axis=0)
+        normalized_points = (points - mean) / std
+        return normalized_points
+
     def forward(self, data_dict):
         batch_size = len(data_dict['instance_points'])
         bts_candidate_point = []
@@ -22,20 +37,29 @@ class FeatureModule(nn.Module):
         bts_relation_point = []
         bts_relation_obbs = []
         bts_relation_mask = []
+        
+        bts_candidate_label_embedding = []
+        bts_relation_label_embedding = []
+        bts_audio_feature = []
+        
         for i in range(batch_size):
             candidate_point = []
             candidate_obbs = []
             candidate_mask = []
+            candidate_label_embedding = []
         
             relation_point = []
             relation_obbs = []
             relation_mask = []
+            relation_label_embedding = []
             instance_point = data_dict['instance_points'][i]
             instance_obb = data_dict['instance_obbs'][i]
             instance_class = data_dict['instance_class'][i]
             # num_obj = len(instance_point)
 
-            audio_feature = data_dict['audio_feature'][i]
+            audio_feature = data_dict['embedded_audio'][i]
+            bts_audio_feature.append(audio_feature)
+            
             audio_class = data_dict['audio_class'][i]
             nel_label = data_dict['nel_label'][i]
             nel_label = ast.literal_eval(nel_label)
@@ -47,19 +71,62 @@ class FeatureModule(nn.Module):
                         candidate_point.append(instance_point[idx][:, :6].tolist())
                         candidate_obbs.append(instance_obb[idx][:6].tolist())
                         candidate_mask.append(1)
+                        candidate_label_embedding.append(self.text_encoder[str(i_class)])
                     else:
                         relation_point.append(instance_point[idx][:, :6].tolist())
                         relation_obbs.append(instance_obb[idx][:6].tolist())
                         relation_mask.append(1)
+                        relation_label_embedding.append(self.text_encoder[str(i_class)])
             # filtering to MAX OBJECT
-            while len(candidate_point) < 8:
+            if len(candidate_point) > 16:
+                candidate_point = candidate_point[:16]
+                candidate_obbs = candidate_obbs[:16]
+                candidate_mask = candidate_mask[:16]
+                candidate_label_embedding = candidate_label_embedding[:16]
+            if len(relation_point) > 16:
+                relation_point = relation_point[:16]
+                relation_obbs = relation_obbs[:16]
+                relation_mask = relation_mask[:16]
+                relation_label_embedding = relation_label_embedding[:16]
+            # filtering to MAX OBJECT
+            while len(candidate_point) < 16:
                 candidate_point.append(np.zeros((1024, 6)).tolist())
                 candidate_obbs.append(np.zeros(6).tolist())
                 candidate_mask.append(0)
-            while len(relation_point) < 8:
+                candidate_label_embedding.append(np.zeros(300).tolist())
+            while len(relation_point) < 16:
                 relation_point.append(np.zeros((1024, 6)).tolist())
                 relation_obbs.append(np.zeros(6).tolist())
                 relation_mask.append(0)
-            print('hih')
+                relation_label_embedding.append(np.zeros(300).tolist())
+
+            bts_candidate_point.append(candidate_point)
+            bts_candidate_obbs.append(candidate_obbs)
+            bts_candidate_mask.append(candidate_mask)
+            bts_candidate_label_embedding.append(candidate_label_embedding)
+
+            bts_relation_point.append(relation_point)
+            bts_relation_obbs.append(relation_obbs)
+            bts_relation_mask.append(relation_mask)
+            bts_relation_label_embedding.append(relation_label_embedding)
+
+
+        
+        bts_candidate_point = torch.tensor(bts_candidate_point).cuda()
+        bts_relation_point = torch.tensor(bts_relation_point).cuda()
+
+        bts_candidate_obbs = torch.tensor(bts_candidate_obbs).cuda() # B x 16 x 6
+        bts_relation_obbs = torch.tensor(bts_relation_obbs).cuda()  # B x 16 x 6
+
+        bts_candidate_mask = torch.tensor(bts_candidate_mask).cuda() # B x 16
+        bts_relation_mask = torch.tensor(bts_relation_mask).cuda() # B x 16
+
+        bts_candidate_label_embedding = torch.tensor(bts_candidate_label_embedding) # 4 x 16 x 300
+        bts_relation_label_embedding = torch.tensor(bts_relation_label_embedding) # 4 x 16 x 300
+
+        bts_audio_feature = torch.stack(bts_audio_feature).cuda() # B x 1 x 1024
+
+        # input_pointnet = torch.cat([bts_candidate_point, bts_relation_point], dim=1).cuda()
+        object_encoding = self.object_encoder(torch.cat([bts_candidate_point, bts_relation_point], dim=1))
 
         return data_dict
