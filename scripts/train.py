026814eaac96f5b7d767b719fc18fe7013b591e6
duccd <caodinhducuet@gmail.com>
2025-05-18 01:22:02 +0900

    add attns

--- scripts/train.py
+++ scripts/train.py
@@ -17,8 +17,8 @@ from lib.solver import Solver
 from lib.config import CONF
 from models.instancerefer import InstanceRefer
 
-SCANREFER_TRAIN = json.load(open(os.path.join(CONF.PATH.DATA, "ScanRefer_filtered_train_with_id_1000.json")))
-SCANREFER_VAL = json.load(open(os.path.join(CONF.PATH.DATA, "ScanRefer_filtered_val_with_id_1000.json")))
+SCANREFER_TRAIN = json.load(open(os.path.join(CONF.PATH.DATA, "ScanRefer_filtered_train_with_id.json")))
+SCANREFER_VAL = json.load(open(os.path.join(CONF.PATH.DATA, "ScanRefer_filtered_val_with_id.json")))
 
 # constants
 DC = ScannetDatasetConfig()
